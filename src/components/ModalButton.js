d66b1f680bf85d5d53c8002044f081947746ecb7
lucywalsh <lucyhannahwalsh@gmail.com>
2024-07-29 20:26:30 +0100

    Buttons and modals for API connections

---
+++ src/components/ModalButton.js
@@ -0,0 +1,38 @@
+import {useState} from 'react';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+export default function ModalButton({buttonText, modalTitle, modalBody, linkButtonText}){
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
+    return(
+        <Row>
+            <Button className="modal-button" onClick={handleShow} style={{backgroundColor:'#9f04ed', borderColor:'#9f04ed'}}>
+                {buttonText}
+            </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        centered
+      >
+        <Modal.Header>
+          <Modal.Title className="text-center modal-title">{modalTitle}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{modalBody}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" style={{backgroundColor:'#9f04ed', borderColor:'#9f04ed'}}>{linkButtonText}</Button>
+        </Modal.Footer>
+      </Modal>
+        </Row>
+    );
+}
--- src/components/TravelPlanner.js
+++ src/components/TravelPlanner.js
@@ -1,9 +1,14 @@
+import {useState} from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import IconButton from './IconButton.js';
+import ModalButton from './ModalButton.js';
+import Button from 'react-bootstrap/Button';
 
 export default function TravelPlanner() {
+    //TODO: pass NWCustomer in as a prop
+    const [NWGCustomer, setNWGCustomer] = useState(true);
 
     return (
         <Container className="planner">
@@ -15,6 +20,19 @@ export default function TravelPlanner() {
             <Row className="mb-3">
                 <h5 class="text-start">Book your trip</h5>
             </Row>
+            {NWGCustomer && <ModalButton
+                buttonText="Set up one-click payments"
+                modalTitle="Quick, easy and secure payments for all your travel needs"
+                modalBody={<div>
+                    <p>Set up a Variable Recurring Payments mandate with your bank today to pay for flights, hotels, trips and more in one-click</p>
+                    <ul>
+                        <li>Set up in seconds in your banking app</li>
+                        <li>Secure payments directly from your bank account, no need to input card details</li>
+                        <li>Your purchases are protected by NatWest</li>
+                    </ul>
+                </div>}
+                linkButtonText="Set up now"
+            ></ModalButton>}
             <Row className="mb-3">
                 <Col>
                     <IconButton
@@ -46,7 +64,22 @@ export default function TravelPlanner() {
             <Row className="mb-3">
                 <h5 class="text-start">Plan your trip</h5>
             </Row>
-            <Row className="mb-3">
+            {NWGCustomer && <ModalButton
+                buttonText="Apply for a Travel Rewards Credit Card"
+                modalTitle="Get Rewards when you spend on holidays abroad and at home"
+                modalBody={<div>
+                    <p>With NatWest's Travel Reward Credit Card, you'll earn Rewards:</p>
+                    <ul>
+                        <li>1% back on eligible travel spending on planes, trains, car rental, ferries, buses, hotel accommodation, travel agents, cruises, campsites and more.</li>
+                        <li>1% to 15% at selected partner retailers.</li>
+                        <li>Or, instead of these, 0.1% back everywhere else.</li>
+                        <li>No foreign transaction fees on purchases abroad.</li>
+                        <li>No annual fee.</li>
+                    </ul>
+                </div>}
+                linkButtonText="Apply now"
+            ></ModalButton>}
+            <Row className="mb-5">
                 <Col>
                     <IconButton
                         id="plan-insurnace"
@@ -62,6 +95,12 @@ export default function TravelPlanner() {
                     ></IconButton>
                 </Col>
             </Row>
+            {!NWGCustomer && 
+            <Row>
+                <p className="subheading">Are you a NatWest, RBS or Ulster Bank Customer?</p>
+                <p>Login via your bank for exclusive offers and ways to pay</p>
+                <Button variant="primary" style={{backgroundColor:'#9f04ed', borderColor:'#9f04ed', width:'40vh', margin:'0 auto'}}>Login securely via your bank</Button>
+            </Row>}
         </Container>
     );
 }
